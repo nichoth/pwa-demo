e9827ba8b27d1c2a51521f7308f682f7e9f2a11e
nichoth <nichoth@gmail.com>
2021-04-06 23:00:42 -0700

    use a coffee picture

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 * https://medium.com/james-johnson/a-simple-progressive-web-app-tutorial-f9708e5f2605
 
+-----------------------------------
+
+* https://www.freecodecamp.org/news/build-a-pwa-from-scratch-with-html-css-and-javascript/#what-is-a-progressive-web-app
+
+
--- src/index.js
+++ src/index.js
@@ -6,3 +6,37 @@ window.onload = () => {
     }
 }
 
+const container = document.querySelector(".container")
+const coffees = [
+  { name: "Perspiciatis", image: "/coffee1.jpg" },
+  { name: "Voluptatem", image: "/coffee1.jpg" },
+  { name: "Explicabo", image: "/coffee1.jpg" },
+  { name: "Rchitecto", image: "/coffee1.jpg" },
+  { name: " Beatae", image: "/coffee1.jpg" },
+  { name: " Vitae", image: "/coffee1.jpg" },
+  { name: "Inventore", image: "/coffee1.jpg" },
+  { name: "Veritatis", image: "/coffee1.jpg" },
+  { name: "Accusantium", image: "/coffee1.jpg" },
+]
+
+// ---------------------------------------------
+
+const showCoffees = () => {
+  let output = ""
+
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+          <div class="card">
+            <img class="card--avatar" src=${image} />
+            <h1 class="card--title">${name}</h1>
+            <a class="card--link" href="#">Taste</a>
+          </div>
+          `)
+  )
+
+  container.innerHTML = output
+}
+
+document.addEventListener("DOMContentLoaded", showCoffees)
+
