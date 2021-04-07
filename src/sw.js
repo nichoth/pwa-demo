28dc31cf433ec91425168ca292c306c7fb722017
nichoth <nichoth@gmail.com>
2021-04-06 22:33:15 -0700

    trying things

--- src/sw.js
+++ src/sw.js
@@ -14,20 +14,25 @@ var filesToCache = [
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function (e) {
     console.log('**install**', e)
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-    console.log('**fetch**', e)
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', function (ev) {
+    console.log('**fetch**', ev)
+
+    ev.respondWith(
+        caches.match(ev.request).then(function (response) {
+            console.log('**caches.match response**', response)
+            if (response === undefined) {
+                console.log('undefined response', ev.request)
+            }
+            return response || fetch(ev.request);
+        })
+    );
 });
 
